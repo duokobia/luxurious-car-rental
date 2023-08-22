96fcdc3d9ab51188afaf776a381dd3edc975c89d
Daniel Okobia <duokobia@gmail.com>
2023-08-22 20:00:36 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,282 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1>LuxuriousRentals</h1>
+        <nav>
+          <ul>
+            <li>Homes</li>
+            <li>Vehicles</li>
+            <li>Blog</li>
+            <li>Contacts</li>
+          </ul>
+        </nav>
+        <button>Dropdown</button>
+        <span>Dotted Icon</span>
+      </header>
+      <section className="landing-banner">
+        <h2>Luxurious Limo Hire</h2>
+        <p>
+          We offer professional car rental & limousone services in our range of
+          high-end vehicles
+        </p>
+        <button>Open Fleet</button>
+        <card>
+          <nav>
+            <ul>
+              <li>Distance</li>
+              <li>Hourly</li>
+              <li>Flat Rate</li>
+            </ul>
+          </nav>
+          <form>
+            <input />
+            <input />
+            <input />
+            <input />
+            <span>Pick up Time</span>
+            <button>Dropdown</button>
+            <button>Dropdown</button>
+            <button>Reserve Now</button>
+          </form>
+        </card>
+      </section>
+      <section className="landing-services">
+        <div>
+          <h3>Services</h3>
+          <span>
+            We invite you to try our services, and we perrsonally guarantee that
+            you will be completely satified.
+          </span>
+        </div>
+        <div>
+          <card>
+            <img src="" alt="" />
+            <h3>Airport transfers</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Intercity trips</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Wedding events</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Business meetings</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+        </div>
+      </section>
+      <section className="landing-our-fleet">
+        <div>
+          <h3>Our Fleet</h3>
+          <span>
+            We invite you to try our services, and we perrsonally guarantee that
+            you will be completely satified.
+          </span>
+        </div>
+        <div className="landing-our-fleet-carousel">
+          <ul>
+            <li>All</li>
+            <li>Luxury</li>
+            <li>Business</li>
+            <li>Crossover</li>
+          </ul>
+          <card>
+            <img src="" alt="" />
+            <h3>Airport transfers</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Intercity trips</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Wedding events</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h3>Business meetings</h3>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+        </div>
+      </section>
+      <section className="landing-why-choose-us">
+        <div>
+          <h3>Why Choose Us</h3>
+          <span>
+            We invite you to try our services, and we perrsonally guarantee that
+            you will be completely satified.
+          </span>
+        </div>
+        <div className="landing-our-fleet-carousel">
+          <card>
+            <img src="" alt="" />
+            <h4>Airport transfers</h4>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h4>Intercity trips</h4>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h4>Wedding events</h4>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+          <card>
+            <img src="" alt="" />
+            <h4>Business meetings</h4>
+            <p>
+              With additional wait time and flight tracking in case of delays,
+              our service is optimized to make every airport transfer a breeze.
+            </p>
+            <button>Read More</button>
+          </card>
+        </div>
+      </section>
+      <section className="landing-only-today">
+        <div>
+          <h3>Only today $75/day</h3>
+          <span>
+            We invite you to try our services, and we perrsonally guarantee that
+            you will be completely satified.
+          </span>
+        </div>
+        <div className="landing-our-fleet-carousel">
+          <card>
+            <div>
+              <h4>Cadillac Escalade</h4>
+              <ul>
+                <li>For Upto 8 Passengers</li>
+                <li>Incredible Sound System</li>
+                <li>Fibre Opticc Lights</li>
+                <li>Bar Area With Fridge</li>
+                <li>Tested Windows</li>
+                <li>Divider With Premium Style</li>
+                <li>Multipurpose Designed Limo</li>
+                <li>Chill Air Conditioning</li>
+              </ul>
+              <button>Reserve Now</button>
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </card>
+        </div>
+      </section>
+      <footer className="app-footer">
+        <div>
+          <h4>LuxuriousRentals</h4>
+          <span>Subscribe to the newletter</span>
+          <input />
+        </div>
+        <nav>
+          <div>
+            <ul>
+              <li className="app-footer-list-heading">Top Cities</li>
+              <li>New York</li>
+              <li>London</li>
+              <li>Berlin</li>
+              <li>Los Angeles</li>
+              <li>Paris</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li className="app-footer-list-heading">Explore</li>
+              <li>Intercity rides</li>
+              <li>Limousine service</li>
+              <li>Chauffeur service</li>
+              <li>Private car service</li>
+              <li>Airport transfer</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li className="app-footer-list-heading">Intercity rides</li>
+              <li>East Hampton - New York</li>
+              <li>New York - Washington</li>
+              <li>New York - Philadephia</li>
+              <li>Abu Dhabi - Dubai</li>
+              <li>London - Birmingham</li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          <span>@ 2023 LuxuriousRentals</span>
+          <ul>
+            <li>Terms</li>
+            <li>Privacy policy</li>
+            <li>Legal notice</li>
+            <li>Accessibility</li>
+          </ul>
+          <div>
+            <span>Icon</span>
+            <span>Icon</span>
+            <span>Icon</span>
+            <span>Icon</span>
+            <span>Icon</span>
+            </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
