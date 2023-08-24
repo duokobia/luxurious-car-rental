b857f4e1c7a40705ef15437c5d40b62fde9b1078
Daniel Okobia <duokobia@gmail.com>
2023-08-24 16:08:53 +0100

    WIP: Create Landing page

--- src/sections/landingBanner/LandingBanner.js
+++ src/sections/landingBanner/LandingBanner.js
@@ -4,47 +4,71 @@ import { NavLink } from "react-router-dom";
 function LandingBanner() {
   return (
     <section className="landingBanner container-fluid px-5">
-      {/* <div className="row"> */}
-      <card className="card landingBanner-card d-flex align-items-center justify-content-center col p-5">
-        <h2>Luxurious Limo Hire</h2>
-        <p className="row d-flex align-items-center justify-content-center">
-          <span>We offer professional car rental & limousine services</span>
-          <span>in our range of high-end vehicles.</span>
-        </p>
-        <button className="col-1 my-4">Open Fleet</button>
-        <div className="d-flex align-items-center justify-content-end">
-          <form className="landingBanner-form border px-3 py-2">
-            <nav className="col">
-              <ul className="d-flex align-items-center justify-content-center list-unstyled pt-2">
-                <li>
-                  <NavLink className="landingBanner-form__button me-2">Distance</NavLink>
-                </li>
-                <li>
-                  <span>Hourly</span>
-                </li>
-                <li>
-                  <span>Flat Rate</span>
-                </li>
-              </ul>
-            </nav>
-            <div className="landingBanner-form__inputs">
-              <div className="row d-flex align-items-center justify-content-center px-4">
-                <input className="row mb-2" />
-                <input className="row mb-2" />
-                <input className="row mb-2" />
-                <input className="row mb-4" />
+      <card className="card landingBanner-card d-flex align-items-center justify-content-start p-5">
+        <h2 className="landingBanner-card__caption mt-3">Luxurious Limo Hire</h2>
+        <small className="d-flex align-items-center justify-content-center flex-column mt-3">
+          <span className="d-inline-block">
+            We offer professional car rental & limousine services
+          </span>
+          <span className="d-inline-block">in our range of high-end vehicles.</span>
+        </small>
+        <button className="col-1 mt-4">Open Fleet</button>
+        <div className="px-1">
+          <div className="row d-flex justify-content-end">
+            <form className="landingBanner-form border px-3 py-2">
+              <nav className="col">
+                <ul className="d-flex align-items-center justify-content-center list-unstyled pt-2">
+                  <li>
+                    <NavLink className="landingBanner-form__button me-2">Distance</NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="landingBanner-form__button me-2">Hourly</NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="landingBanner-form__button me-2">FlatRate</NavLink>
+                  </li>
+                </ul>
+              </nav>
+              <div className="landingBanner-form__inputs">
+                <div className="row d-flex align-items-center justify-content-center px-4">
+                  <input
+                    className="row mb-3"
+                    type="text"
+                    placeholder="Pick Up Address"
+                    aria-label="default input example"
+                  />
+                  <input
+                    className="row mb-3"
+                    type="text"
+                    placeholder="Drop Off Address"
+                    aria-label="default input example"
+                  />
+                  <input
+                    className="row mb-3"
+                    type="text"
+                    placeholder="One way"
+                    aria-label="default input example"
+                  />
+                  <input
+                    className="row mb-4"
+                    type="text"
+                    placeholder="06/04/2023"
+                    aria-label="default input example"
+                  />
+                </div>
+                <span className="landingBanner-form__cta">
+                  <span className="col-4">Pick up Time</span>
+                  <button className="col-4">Drop</button>
+                  <button className="col-4">Drop</button>
+                </span>
               </div>
-            </div>
-            <div className="landingBanner-form__cta">
-              <span className="pe-3">Pick up Time</span>
-              <button className="me-2">Drop</button>
-              <button className="me-2">Drop</button>
-              <button>Reserve Now</button>
-            </div>
-          </form>
+              <div className="pt-4 pb-2 ps-2">
+                <button className="col">Reserve Now</button>
+              </div>
+            </form>
+          </div>
         </div>
       </card>
-      {/* </div> */}
     </section>
   );
 }
--- src/sections/landingServices/LandingServices.js
+++ src/sections/landingServices/LandingServices.js
@@ -2,51 +2,63 @@ import React from "react";
 
 function LandingServices() {
   return (
-    <section className="landing-services">
-      <div>
-        <h3>Services</h3>
-        <span>
-          We invite you to try our services, and we perrsonally guarantee that you will be
-          completely satified.
-        </span>
+    <section className="landing-services container py-5">
+      <div className="col-8 offset-3 d-flex align-items-center justify-content-center my-5">
+        <h3 className="pe-5">Services</h3>
+        <small className="row">
+          <span>We invite you to try our services, and we personally</span>
+          <span>guarantee that you will be completely satified.</span>
+        </small>
       </div>
       <div>
-        <card>
-          <img src="" alt="" />
-          <h3>Airport transfers</h3>
-          <p>
-            With additional wait time and flight tracking in case of delays, our service is
-            optimized to make every airport transfer a breeze.
-          </p>
-          <button>Read More</button>
-        </card>
-        <card>
-          <img src="" alt="" />
-          <h3>Intercity trips</h3>
-          <p>
-            With additional wait time and flight tracking in case of delays, our service is
-            optimized to make every airport transfer a breeze.
-          </p>
-          <button>Read More</button>
-        </card>
-        <card>
-          <img src="" alt="" />
-          <h3>Wedding events</h3>
-          <p>
-            With additional wait time and flight tracking in case of delays, our service is
-            optimized to make every airport transfer a breeze.
-          </p>
-          <button>Read More</button>
-        </card>
-        <card>
-          <img src="" alt="" />
-          <h3>Business meetings</h3>
-          <p>
-            With additional wait time and flight tracking in case of delays, our service is
-            optimized to make every airport transfer a breeze.
-          </p>
-          <button>Read More</button>
-        </card>
+        <div className="servicesCard-row row gx-5 px-5 mb-5">
+          <card className="card servicesCard col d-flex align-items-center justify-content-center flex-row p-3">
+            <img src="" alt="Airport-transfers" />
+            <div className="card-body">
+              <h4>Airport transfers</h4>
+              <p>
+                With additional wait time and flight tracking in case of delays, our service is
+                optimized to make every airport transfer a breeze.
+              </p>
+              <button>Read More</button>
+            </div>
+          </card>
+          <card className="card servicesCard col d-flex align-items-center justify-content-center flex-row p-3">
+            <img src="" alt="Intercity-trips" />
+            <div className="card-body">
+              <h4>Intercity trips</h4>
+              <p>
+                With additional wait time and flight tracking in case of delays, our service is
+                optimized to make every airport transfer a breeze.
+              </p>
+              <button>Read More</button>
+            </div>
+          </card>
+        </div>
+        <div className="servicesCard-row row gx-5 px-5 mb-5">
+          <card className="card servicesCard col d-flex align-items-center justify-content-center flex-row p-3">
+            <img src="" alt="Wedding-events" />
+            <div className="card-body">
+              <h4>Wedding events</h4>
+              <p>
+                With additional wait time and flight tracking in case of delays, our service is
+                optimized to make every airport transfer a breeze.
+              </p>
+              <button>Read More</button>
+            </div>
+          </card>
+          <card className="card servicesCard col d-flex align-items-center justify-content-center flex-row p-3">
+            <img src="" alt="Business-meetings" />
+            <div className="card-body">
+              <h4>Business meetings</h4>
+              <p>
+                With additional wait time and flight tracking in case of delays, our service is
+                optimized to make every airport transfer a breeze.
+              </p>
+              <button>Read More</button>
+            </div>
+          </card>
+        </div>
       </div>
     </section>
   );
